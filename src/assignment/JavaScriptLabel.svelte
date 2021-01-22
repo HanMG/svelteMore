8cfd2ccc3bf7242c21d35c81f058da39cf7cbd0b
hanmoongoo <hanmoongoo@gmail.com>
2021-01-22 22:53:27 +0900

    반응성 구문

---
+++ src/assignment/JavaScriptLabel.svelte
@@ -0,0 +1,29 @@
+<script>
+    // Svelte가 아닌 , Javascript 문법 label
+
+    loop1:
+    for(let i = 1; i < 4; i += 1){
+        loop2:
+        for(let k = 1; k < 4; k += 1){
+            if(i == 2){
+                // loop1 자체가 멈추게된다.
+                break loop1
+            }
+            console.log(`${i}-${k}`)
+        }
+        console.log('-----------')
+    }
+
+    hmg: {
+        console.log('a')
+        break hmg
+        console.log('b')
+    }
+
+    // Svelte 반응성 구문!
+    let count = 0
+    $: {
+        console.log(count)
+    }
+
+</script>
---
+++ src/assignment/Reactive.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { tick } from 'svelte'
+    let count = 0
+    let double = 0
+    
+    $: {
+        double = count * 2
+		console.log('double!')
+    }
+
+    //$: double = count * 2
+
+    async function assign() {
+        count += 1
+        console.time('timer')
+        await tick()
+        console.timeEnd('timer')
+        console.log(double)
+    }
+
+    // 실행순서
+    // count += 1
+    // await tick()
+    // $:double = count * 2
+    // console.log(double)
+
+</script>
+
+
+
+
+<button on:click={assign}>Assign!</button>
+<h2>{count}</h2>
+<h2>{double}</h2>
