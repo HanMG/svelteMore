07609d61de4c5b6d7a375612a82cd7cd9d8bf712
hanmoongoo <hanmoongoo@gmail.com>
2021-01-28 17:47:05 +0900

    Async 이해

---
+++ src/async/Async.svelte
@@ -0,0 +1,60 @@
+<script>
+    // function a(cb) {
+    //     setTimeout(() => {
+    //         console.log('a')
+    //         cb()
+    //     }, 1000)        
+    // }
+
+   function a() {
+       return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('a')       
+                resolve()     
+            }, 1000)      
+       })
+   }
+
+   function b() {
+       return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('b')       
+                resolve()     
+            }, 1000)      
+       })
+   }
+
+   function c() {
+       return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('c')       
+                resolve()     
+            }, 1000)      
+       })
+   }
+
+   function d() {
+       return new Promise((resolve) => {
+            setTimeout(() => {
+                console.log('d')       
+                resolve()     
+            }, 1000)      
+       })
+   }
+//      ES8 이전
+//    a()
+//     .then(() => b())
+//     .then(() => c())
+//     .then(() => d())
+
+   // ES8 이후 async await
+   async function asyncFn() {
+       await a()
+       await b()
+       await c()
+       await d()
+   }
+
+   asyncFn()
+
+</script>
---
+++ src/async/Async1.svelte
@@ -0,0 +1,55 @@
+<script>
+    let isError = false
+
+   function a() {
+       // 대기(pending): '이행' 하거나 '거부' 되지 않은 초기 상태.
+       return new Promise((resolve, reject) => {
+           if(isError){
+               // 거부(rejected): 연산이 실패함
+            //    reject('Sorry...')            
+                reject(new Error('Sorry'))
+                return 
+           }
+            // 이행(fulfilled): 연산이 성공적으로 완료됨
+            setTimeout(() => {
+                console.log('a')       
+                resolve('HMG')     
+            }, 1000)      
+       })
+   }
+
+
+    // 이행(fulfilled): 연산이 성공적으로 완료됨
+    // 거부(rejected): 연산이 실패함
+
+
+//    a()
+// 이행(fulfilled): 연산이 성공적으로 완료됨
+//     .then((res) => {
+//         console.log(res)
+//         console.log('b')
+//     })
+// 거부(rejected): 연산이 실패함
+//     .catch((err) => {
+//         console.log(err)
+//     })
+//     .finally(() => {
+//         console.log('Done')
+//     })
+    
+    async function asyncFn() {
+        try {
+            const res = await a()            
+            console.log(res)
+            console.log('b')
+        }catch(err){
+            console.log(err.message)
+        }finally{
+            console.log('Done')
+        }       
+    }
+
+    asyncFn()
+  
+
+</script>
